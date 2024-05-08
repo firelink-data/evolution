e6e74a39f38f33f6dcf41ddd098174bad11f45c9
wilhelmagren <wilhelmagren98@gmail.com>
2024-05-09 00:58:12 +0200

    [feat] test and verify written parquet file

---
+++ tools/verify-parquet.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import argparse
+import pyarrow.parquet as pq
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="verify-parquet",
+        description="Print metadata and some rows from a .parquet file.",
+        epilog="Happy hacking! 👋🥳",
+    )
+
+    parser.add_argument(
+        "file", help="The .parquet file to verify.",
+    )
+
+    args = parser.parse_args()
+    file = args.file
+
+    parquet_file = pq.ParquetFile(file)
+    print("="*60)
+    print(parquet_file.metadata)
+    print("="*60)
+    print(parquet_file.schema)
+    print("="*60)
+
+    table = pq.read_table(file)
+    df = table.to_pandas()
+    print(df.head())
+    print("="*60)
+    print("\nDone!\n")
